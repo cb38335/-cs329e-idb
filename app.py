d485d1fc5f7eb0a42b48f1d6e96e51dc3d0a992f
Rin <pomeryt@naver.com>
2017-04-17 03:10:42 -0500

    refactoring in progress (not done yet)

--- app.py
+++ app.py
@@ -1,12 +1,55 @@
-__author__ = 'Grecia'
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
+def entry():
+    return render_template("index.html")
+
+@app.route("/index.html")
 def index():
-    return "Text :)"
+    return render_template("index.html")
+
+@app.route("/globalComponent/topNavigator.html")
+def topNavigator():
+    return render_template("globalComponent/topNavigator.html")
+
+@app.route("/mainPage/creator-main.html")
+def creatorMain():
+    return render_template("mainPage/creator-main.html")
+
+@app.route("/mainPage/creator-main2.html")
+def creatorMain2():
+    return render_template("mainPage/creator-main2.html")
+
+@app.route("/mainPage/cname-main.html")
+def cNameMain():
+    return render_template("mainPage/cname-main.html")
+
+@app.route("/mainPage/cname-main2.html")
+def cNameMain2():
+    return render_template("mainPage/cname-main2.html")
+
+@app.route("/mainPage/country-main.html")
+def countryMain():
+    return render_template("mainPage/country-main.html")
+
+@app.route("/mainPage/country-main2.html")
+def countryMain2():
+    return render_template("mainPage/country-main2.html")
+
+@app.route("/about/about.html")
+def about():
+    return render_template("about/about.html")
+
+@app.route("/creator/creator1.html")
+def creator1():
+    return render_template("creator/creator1.html")
 
 if __name__ == "__main__":
-    app.run("138.197.24.180", "80")
+    # Actual Web Address
+    #app.run("138.197.24.180", "80")
+
+    # For Test
+    app.run()
     
