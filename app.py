401a2ad19c295b1ea2075428d3828118b2a3dea0
Beom Kim <pomeryt@cs.utexas.edu>
2017-04-17 16:33:50 -0500

    completed app.py

--- app.py
+++ app.py
@@ -46,6 +46,74 @@ def about():
 def creator1():
     return render_template("creator/creator1.html")
 
+@app.route("/creator/creator2.html")
+def creator2():
+    return render_template("creator/creator2.html")
+
+@app.route("/creator/creator3.html")
+def creator3():
+    return render_template("creator/creator3.html")
+
+@app.route("/creator/creator4.html")
+def creator4():
+    return render_template("creator/creator4.html")
+
+@app.route("/creator/creator5.html")
+def creator5():
+    return render_template("creator/creator5.html")
+
+@app.route("/creator/creator6.html")
+def creator6():
+    return render_template("creator/creator6.html")
+
+@app.route("/cartoon/Arthur.html")
+def cartoon1():
+    return render_template("/cartoon/Arthur.html")
+
+@app.route("/cartoon/FullMetalAlchemist.html")
+def cartoon2():
+    return render_template("/cartoon/FullMetalAlchemist.html")
+
+@app.route("/cartoon/Gumball.html")
+def cartoon3():
+    return render_template("/cartoon/Gumball.html")
+
+@app.route("/cartoon/Masha.html")
+def cartoon4():
+    return render_template("/cartoon/Masha.html")
+
+@app.route("/cartoon/Miraculous.html")
+def cartoon5():
+    return render_template("/cartoon/Miraculous.html")
+
+@app.route("/cartoon/TheSimpsons.html")
+def cartoon6():
+    return render_template("/cartoon/TheSimpsons.html")
+
+@app.route("/countryOfOrigin/canada.html")
+def country1():
+    return render_template("/countryOfOrigin/canada.html")
+
+@app.route("/countryOfOrigin/france.html")
+def country2():
+    return render_template("/countryOfOrigin/france.html")
+
+@app.route("/countryOfOrigin/japan.html")
+def country3():
+    return render_template("/countryOfOrigin/japan.html")
+
+@app.route("/countryOfOrigin/russia.html")
+def country4():
+    return render_template("/countryOfOrigin/russia.html")
+
+@app.route("/countryOfOrigin/uk.html")
+def country5():
+    return render_template("/countryOfOrigin/uk.html")
+
+@app.route("/countryOfOrigin/usa.html")
+def country6():
+    return render_template("/countryOfOrigin/usa.html")
+
 if __name__ == "__main__":
     # Actual Web Address
     #app.run("138.197.24.180", "80")
